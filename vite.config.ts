cb59a727315c76edf7f79911e324bb7df50325a3
Maksym <max669@ukr.net>
2025-09-10 19:34:40 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/03-react-movies/',
-})
+  base: '/03-react-movies/'
+});
